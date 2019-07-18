004b828a4a8398561f2d507b70ac9a808eb38879
Bhaumik Patel <bhaumik55231@gmail.com>
2019-07-18 09:28:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Testing google source repo';
+  let message = req.query.message || req.body.message || 'Testing google source repo...!';
   res.status(200).send(message);
 };
